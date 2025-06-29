16212aba11834c1815582668a293c6879bd4eac8
n_n <nursultanzhantuar@gmail.com>
2025-06-30 01:21:29 +0500

    Add secret key

--- src/auth/config.py
+++ src/auth/config.py
@@ -3,6 +3,6 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class AuthSettings(BaseSettings):
     SECRET : str
 
-    model_config = SettingsConfigDict(env_file=".env")
+    model_config = SettingsConfigDict(env_file="auth.env")
 
 SECRET = AuthSettings().SECRET
